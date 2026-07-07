84dd90611e92078b9a362ab062dd2ffffa727127
kirkchu <kirkchu@gmail.com>
2026-07-07 09:01:52 +0800

    Update YOLO classification training

--- src/yolo_cls_train.py
+++ src/yolo_cls_train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 model = YOLO('src/../model/yolo26n-cls.pt')
 save_dir = 'src/../model/runs/classify'
 model.train(
-    data='clsdataset',
+    data='src/clsdataset',
     epochs=50,
     imgsz=224,
     batch=32,
